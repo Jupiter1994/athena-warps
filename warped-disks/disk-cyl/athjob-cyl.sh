04d5945672f5f9a7fa325764fc4e68b348a73867
Jupiter Ding <thejupiterding@gmail.com>
2026-02-05 14:09:14 -0600

    reorganized files

---
+++ warped-disks/disk-cyl/athjob-cyl.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#SBATCH --account=b1094 ## Required: your allocation/account name, i.e. eXXXX, pXXXX or bXXXX
+#SBATCH --partition=ciera-std ## Required: (buyin, short, normal, long, gengpu, genhimem, etc)
+#SBATCH --time=2:00:00 ## Required: How long will the job need to run (remember different partitions have restrictions on this parameter)
+#SBATCH --nodes=1 ## how many computers/nodes do you need (no default)
+#SBATCH --ntasks-per-node=4 ## how many cpus or processors do you need on per computer/node (default value 1)
+#SBATCH --mem=1G ## how much RAM do you need per computer/node (this affects your FairShare score so be careful to not ask for more than you need))
+#SBATCH --job-name=disk-cyl ## When you run squeue -u
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=jupiterding2029@u.northwestern.edu
+
+# load in modules
+module load python-anaconda3
+module load intel/2024.0
+module load mpi/intel-mpi-5.1.3.258
+module load hdf5/1.10.7-openmpi-intel-2021.4.0
+
+# configure problem
+python ../../configure.py --prob=disk --coord=cylindrical --eos=isothermal -hdf5 -h5double -mpi --hdf5_path /hpc/software/spack_v20d1/spack/opt/spack/linux-rhel7-x86_64/intel-2021.4.0/hdf5-1.10.7-exmishiaff3ugqbeeimkqvnwrir76sxh -omp --cflag=-qno-openmp-simd
+make clean
+make -j 4
+
+# run the sim
+mpiexec -n 4 ../../bin/athena -i athinput.disk_cyl
